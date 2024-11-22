6069a2aa675b7835770ed524d7f7a0b4e988ae6d
Pranay Jagtap <pranays.jagtap@gmail.com>
2024-11-22 06:23:01 +0000

    1st final commit-updated personal website link.

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ if classify_btn and st.session_state.preprocessed_img is not None:
     classify_img()
 
 st.markdown("""---""")
-st.markdown("Created by [Pranay Jagtap](https://pranayjagtap06.github.io)")
+st.markdown("Created by [Pranay Jagtap](https://pranayjagtap.netlify.app)")
 
 # Get the base64 string of the image
 img_base64 = get_image_base64("assets/pranay_sq.jpg")
@@ -131,6 +131,6 @@ html_code = f"""
 # Display the circular image
 st.markdown(html_code, unsafe_allow_html=True)
 # st.image("assets/pranay_sq.jpg", width=125)
-st.markdown("Electrical Engineer ⚡ | Python enthusiast 🐍 | Machine learning explorer 🤖 "\
-            "<br>📍 Nagpur, Maharashtra, India", unsafe_allow_html=True)
+st.markdown("Electrical Engineer | Machine learning Enthusiast "\
+            "<br>Nagpur, Maharashtra, India", unsafe_allow_html=True)
 
